b6e93d5f6eb240e63308a2bda7af807807d66235
Alex Eesley <alexandra.eesley@gmail.com>
2020-11-29 10:24:41 -0600

    added models folder with book schema

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./style.css";
 
-function SearchBar() {
+function SearchBar(props) {
   return (
     <div class="jumbotron jumbotron-fluid">
     <div class="container">
         <h3 class="">Book Search</h3>
         <p class="lead">Enter a book title to search!</p>
-        <form class="example" action="action_page.php">
-            <input type="text" placeholder="Search.." name="search"></input>
+        <form 
+            class="example" action="action_page.php">
+            <input 
+                type="text" 
+                placeholder="Search.." 
+                name="search">
+                </input>
             <button type="submit">Search</button>
         </form>
     </div>
