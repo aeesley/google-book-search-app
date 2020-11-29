dad382191c072fe69e4b8c3f2bcdbef37d919e8f
Alex Eesley <alexandra.eesley@gmail.com>
2020-11-29 09:22:59 -0600

    added header component and added successfully to both pages

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Google Book Search</h1>
+        <p class="lead">Search for and Save Books of Interest!</p>
+    </div>
+    </div>
+  );
+}
+
+export default Header;
--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./style.css";
+import Header from "../../components/Header/index.js";
 
 function Search(props) {
   return (
     <div className="card">
+    <Header />
       <h1>Search PAGE!</h1>
 
     </div>
