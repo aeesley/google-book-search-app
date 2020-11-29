7ded63cdcca3d945d55f70e63ac062b3392ee7e1
Alex Eesley <alexandra.eesley@gmail.com>
2020-11-29 09:33:56 -0600

    added search bar component with search input bar for user to search books

---
+++ client/src/components/SearchBar/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function SearchBar() {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Book Search</h1>
+        <p class="lead">Enter a book title to search!</p>
+        <form class="example" action="action_page.php">
+            <input type="text" placeholder="Search.." name="search"></input>
+            <button type="submit">Search</button>
+        </form>
+    </div>
+    </div>
+  );
+}
+
+export default SearchBar;
--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./style.css";
 import Header from "../../components/Header/index.js";
+import SearchBar from "../../components/SearchBar/index.js";
 
 function Search(props) {
   return (
     <div className="card">
     <Header />
-      <h1>Search PAGE!</h1>
-
-    </div>
-  
+    <SearchBar />
+  </div>
   );
 }
 
